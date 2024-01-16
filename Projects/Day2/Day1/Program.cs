c1b3e28add92c897592b69909617c9bc38a74b80
Mirolim Majidov <Mirolim@silkroadprofessionals.com>
2024-01-16 20:54:32 +0500

    Work with delegates and Events

--- Projects/Day2/Day1/Program.cs
+++ Projects/Day2/Day1/Program.cs
@@ -1,37 +1,42 @@
 ﻿// Declaration of delegate
 using System;
 
-delegate void Calculator(int a, int b);
+delegate void ActionCalculator(int a, int b);
+delegate int FuncCalculator(int a, int b);
 
 class Program
 {
     static void Main()
     {
-        Calculator calculator = Addition;
-        calculator(10, 5);//Result: 15
+        //WorkWithStandardEvent();
+        //WorkWithCustomEvent();
+        ActionCalculator voidCalculator = Addition;
+        Action<int, int> voidCalculatorWithAction = Addition;
+
+        FuncCalculator nonVoidCalculator = FuncAddition;
+        Func<int, int, int> nonVoidCalculatorWithFunc = FuncAddition;
+
+        //Executing method
+        voidCalculator(10, 5);//Result: 15
+        nonVoidCalculator(10, 5);//Result: 15
 
         //Recalling delegate
-        calculator = Subtraction;
-        calculator(10, 5);//Result: 5
-
-        Console.ReadLine();
+        nonVoidCalculator = (a, b) => a + b;
+        voidCalculator = Subtraction;
 
         //Methods for operations
-        void Addition(int a, int b)
-        {
-            Console.WriteLine(a + b);
-        }
 
-        void Subtraction(int a, int b)
-        {
-            Console.WriteLine(a - b);
-        }
+        int FuncAddition(int a, int b) => a + b;
+        void Addition(int a, int b) => Console.Write(a + b);
+
+        void Subtraction(int a, int b) => Console.WriteLine(a - b);
     }
+
 
     // Declaration of event
     public static event EventHandler MyEvent;
 
-    static void Main2()
+    static void WorkWithStandardEvent()
     {
         // Subscribing to the event
         MyEvent += (sender, e) => Console.WriteLine("Event triggered!");
@@ -39,4 +44,46 @@ class Program
         // Raising the event
         MyEvent?.Invoke(null, EventArgs.Empty);
     }
+
+    static void WorkWithCustomEvent()
+    {
+        var user = new User();
+        user.Balance = 10;
+        user.TransactionCompleted += (sender, e) => TransactionNotify(sender, e);
+
+        user.Transaction(15);
+        user.Transaction(-20);
+    }
+
+    static void TransactionNotify(object sender, TransactionCompletedEventArgs eventArgs)
+    {
+
+    }
 }
+
+public class User
+{
+    public double Balance { get; set; }
+    public event EventHandler<TransactionCompletedEventArgs> TransactionCompleted;
+
+    public void Transaction(int moneyToTransaction)
+    {
+        Balance += moneyToTransaction;
+
+        // Raise the event with custom data
+        //TransactionCompleted?.Invoke(this, EventArgs.Empty);
+        TransactionCompleted?.Invoke(this, new TransactionCompletedEventArgs(moneyToTransaction, Balance));
+    }
+}
+
+public class TransactionCompletedEventArgs : EventArgs
+{
+    public double TransferedMoney { get; private set; }
+    public double AccountBalance { get; private set; }
+    public TransactionCompletedEventArgs(double transferedMoney, double accountBalance)
+    {
+        TransferedMoney = transferedMoney;
+        AccountBalance = accountBalance;
+    }
+}
+
--- Projects/Day2/TestPro/Class1.cs
+++ Projects/Day2/TestPro/Class1.cs
@@ -1,16 +1,35 @@
 ﻿namespace TestPro
 {
+    class Program
+    {
+        static void Main()
+        {
+            var user = new User();
+            user.Balance = 10;
+            user.TransactionCompleted += (sender, e) => TransactionNotify(sender, e);
+
+
+            user.Transaction(15);
+            user.Transaction(-20);
+        }
+
+        static void TransactionNotify(object sender, EventArgs eventArgs)
+        {
+
+        }
+    }
     public class User
     {
         public double Balance { get; set; }
-        public event EventHandler<TransactionCompletedEventArgs> TransactionCompleted;
+        public event EventHandler<EventArgs> TransactionCompleted;
 
         public void Transaction(int moneyToTransaction)
         {
-            Balance -= moneyToTransaction;
+            Balance += moneyToTransaction;
 
             // Raise the event with custom data
-            TransactionCompleted?.Invoke(this, new TransactionCompletedEventArgs(moneyToTransaction, Balance));
+            TransactionCompleted?.Invoke(this, EventArgs.Empty);
+            //TransactionCompleted?.Invoke(this, new TransactionCompletedEventArgs(moneyToTransaction, Balance));
         }
     }
 
